6e6d4dde9812560101b5fdf87bcac601422ef3a0
Jerald Jesudasan <96613771+jerald1608@users.noreply.github.com>
2022-11-24 21:08:24 +0530

    Update ex1_ftp_server.py

--- ex1/ex1_ftp_server.py
+++ ex1/ex1_ftp_server.py
@@ -1,16 +1,21 @@
 import socket
 import sys
-
 HOST = "localhost"
 PORT = 12345
-s = socket.socket(socket.AF_INET,   socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print(" Connected with Server")
-f_send = "file_to_send.txt"
-f=open(f_send, "rb") 
-print("Sending file...")
-data = f.read()
-s.sendall(data)
-s.close()
-print("Disconnected")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+print("Listening ...")
+conn, addr = s.accept()
+print(" Client connected: ", addr)
+f = open("file_received.txt", "wb")
+while True:
+    data = conn.recv(4096)
+    if not data:
+        break
+    f.write(data)
+f.close()
+print("Download complete!")
+conn.close()
+print("Client disconnected")
 sys.exit(0)
